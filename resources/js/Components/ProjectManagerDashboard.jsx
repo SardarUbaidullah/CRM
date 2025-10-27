afbea8f70cd2c0ff0d82d8d628a3e7e761f915cb
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-10-28 02:08:21 +0500

    merge conflicts

--- resources/js/Components/ProjectManagerDashboard.jsx
+++ resources/js/Components/ProjectManagerDashboard.jsx
@@ -1,9 +1,5 @@
 // resources/js/Components/Dashboard/KanbanBoard.jsx
-<<<<<<< HEAD
 import React, { useState, useRef, useEffect,useMemo } from 'react';
-=======
-import React, { useState, useRef, useEffect, useMemo } from 'react';
->>>>>>> 21672d14f255e5f109d81a25046b359c33c6b80e
 import { motion, AnimatePresence } from 'framer-motion';
 import {
     MessageSquare,
@@ -129,22 +125,8 @@ const calculateTeamProductivity = () => {
             return completedDate > weekAgo;
         }).length;
 
-<<<<<<< HEAD
-        const calculateTeamProductivity = () => {
-        const completedThisWeek = tasks.done.filter(task => {
-            const completedDate = new Date(task.updatedAt);
-            const weekAgo = new Date();
-            weekAgo.setDate(weekAgo.getDate() - 7);
-            return completedDate > weekAgo;
-        }).length;
-
         return Math.min(100, Math.round((completedThisWeek / tasks.done.length) * 100)) || 0;
     };
-
-=======
-        return Math.min(100, Math.round((completedThisWeek / tasks.done.length) * 100)) || 0;
-    };
->>>>>>> 21672d14f255e5f109d81a25046b359c33c6b80e
     // PM-specific stats
     const projectStats = useMemo(() => {
 
